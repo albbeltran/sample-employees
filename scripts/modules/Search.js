5c2605139c9bb504ce94d53dc5665f8226cac5aa
Alberto Beltran <albertobeltran@protonmail.com>
2023-10-25 17:18:22 -0600

    implemented clear filter on employee search

--- scripts/modules/Search.js
+++ scripts/modules/Search.js
@@ -3,59 +3,68 @@ import Render from './Render.js';
 export default class Search {
     constructor() {
         this.searchForm = document.querySelector('#search-form');
-        this.id = document.querySelector('#emp_search');
+        this.btnClear = document.querySelector('#btn-clear');
+        this.btnClear.addEventListener('click', this.clear);
         this.events();
     }
 
     events() {
         this.searchForm.addEventListener('submit', (e) => {
+            // every submit, update id
+            this.id = document.querySelector('#emp_search');
             e.preventDefault();
-            console.log(this.id.value)
+            this.formSubmitHandler();
+        })
+
+        this.btnClear.addEventListener('click', () => {
+            // if clear, then there is no id to search
+            this.id = undefined;
             this.formSubmitHandler();
         })
     }
 
     selectPath() {
         if (this.id !== undefined) this.path = `http://localhost:3000/empleado/${this.id.value}`;
+        // if id is undefined, select all elements
         else this.path = 'http://localhost:3000/empleado/'
     }
 
     formSubmitHandler() {
-        this.idHandler();
+        console.log(this.id)
+        this.selectPath();
 
-        if (this.id.errors === false) {
-            // this.form.submit();
+        if (this.id) {
+            this.idHandler();
+            if (this.id.errors) return;
+            this.id.value = '';
+        }
 
-            this.selectPath()
-            console.log(this.path)
+        fetch(this.path, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    res.json().then(employees => {
+                        Render.clearTable();
 
-            fetch(this.path, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                        if (!employees.length) Render.insertNewRowTable(employees, true)
+                        else {
+                            employees.forEach(employee => {
+                                Render.insertNewRowTable(employee);
+                            })
+                        }
+                    }
+                    );
+                } else if (res.status === 400) {
+                    alert('The employee does not exists in database.');
+                    console.error(`ERROR. Status code: ${res.status}`);
                 }
             })
-                .then(res => {
-                    if (res.status === 200) {
-                        res.json().then(employees => {
-                            Render.clearTable();
-
-                            if (!employees.length) Render.insertNewRowTable(employees, true)
-                            else {
-                                employees.forEach(employee => {
-                                    Render.insertNewRowTable(employee);
-                                })
-                            }
-                        }
-                        );
-                    } else if (res.status === 400) {
-                        alert('The employee does not exists in database.');
-                        console.error(`ERROR. Status code: ${res.status}`);
-                    }
-                })
-                .catch(err => console.error(`Request error: ${err}`))
-        }
+            .catch(err => console.error(`Request error: ${err}`))
     }
 
     idHandler() {
