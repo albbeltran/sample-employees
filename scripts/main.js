d5105801a18012e288e28f6f3844caae760063c0
Alberto Beltran <albertobeltran@protonmail.com>
2023-10-30 18:11:21 -0600

    login using fetch to validate input value

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+import Login from './modules/Login.js';
+import Add from './modules/Add.js';
 import Update from './modules/update.js';
 import Delete from './modules/Delete.js';
 import Search from './modules/Search.js';
-import Add from './modules/Add.js';
 
+if(document.querySelector('#login-form')) new Login();
 if(document.querySelector('#add-form')) new Add();
 if(document.querySelector('#update-btn')) new Update();
 if(document.querySelector('#delete-btn')) new Delete();
